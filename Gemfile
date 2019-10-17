3e91c7c263f102f6e532a1a443626867f2d98476
Chi Trung Nguyen <git@ctnguyen.net>
2019-10-17 22:54:58 +0200

    rubocop power

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 #
-gem "nokogiri"
-gem "httparty"
-gem "pry"
-gem "http"
+gem 'http'
+gem 'httparty'
+gem 'nokogiri'
+gem 'pry'
--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'HTTParty'
 require 'Nokogiri'
 require 'json'
@@ -8,7 +10,8 @@ require 'http'
 
 # helper
 def suppress_output
-  original_stdout, original_stderr = $stdout.clone, $stderr.clone
+  original_stdout = $stdout.clone
+  original_stderr = $stderr.clone
   $stderr.reopen File.new('/dev/null', 'w')
   $stdout.reopen File.new('/dev/null', 'w')
   yield
@@ -19,60 +22,59 @@ end
 #######
 
 # constants and var init
-HYPEM_TEXT=" - search Hype Machine for this artist"
-track, artist = ""
+HYPEM_TEXT = ' - search Hype Machine for this artist'
+track, artist = ''
 
-#parsed_page = HTTParty.get("https://hypem.com/napcae")
-parsed_page = File.open("index.html")
+# parsed_page = HTTParty.get("https://hypem.com/napcae")
+parsed_page = File.open('index.html')
 hypem_loved = Nokogiri::HTML(parsed_page)
 
-def get_track_info(artist,track,title_count = 1,*args)
-	__DEEZER_API_ENDPOINT="https://api.deezer.com/search?q="
-	# creating api call for deezer, search for artist and tracks scraped from hypem loved page
-	# TODO: Replace with actual variables from step above
-	query = __DEEZER_API_ENDPOINT + "artist:" + "\"#{artist}\"" + "track:" + "\"#{track}\"" + "&limit=#{title_count}&order=RANKING"
+def get_track_info(artist, track, title_count = 1, *_args)
+  __DEEZER_API_ENDPOINT = 'https://api.deezer.com/search?q='
+  # creating api call for deezer, search for artist and tracks scraped from hypem loved page
+  # TODO: Replace with actual variables from step above
+  query = __DEEZER_API_ENDPOINT + 'artist:' + "\"#{artist}\"" + 'track:' + "\"#{track}\"" + "&limit=#{title_count}&order=RANKING"
 
-	deezer_query = HTTParty.get(query).to_s
-	parsed = JSON.parse(deezer_query)
-	parsed = parsed["data"]
+  deezer_query = HTTParty.get(query).to_s
+  parsed = JSON.parse(deezer_query)
+  parsed = parsed['data']
 
-	parsed.take(title_count).each do | deezer_search_response |
-		link = deezer_search_response["link"]
-		title = deezer_search_response["title"]
-		artist_name = deezer_search_response["artist"]["name"]
-		album_name =  deezer_search_response["album"]["title"]
-		
-		# for debugging
-		debug = "# " + artist_name + " - " + title + " from " + album_name
+  parsed.take(title_count).each do |deezer_search_response|
+    link = deezer_search_response['link']
+    title = deezer_search_response['title']
+    artist_name = deezer_search_response['artist']['name']
+    album_name =  deezer_search_response['album']['title']
 
-		return [link,debug]
-	end
+    # for debugging
+    debug = '# ' + artist_name + ' - ' + title + ' from ' + album_name
+
+    return [link, debug]
+  end
 end
 
-#puts get_track_info("eminem","lose yourself")[0]
+# puts get_track_info("eminem","lose yourself")[0]
 
 # parse loved songs from hypem loved page
-hypem_loved.css('#track-list').css('.track_name').map do | track_item |
-  suppress_output {
-  artist = track_item.css(".artist").attribute('title').text
-  artist = artist.gsub(HYPEM_TEXT,"")
+hypem_loved.css('#track-list').css('.track_name').map do |track_item|
+  suppress_output do
+    artist = track_item.css('.artist').attribute('title').text
+    artist = artist.gsub(HYPEM_TEXT, '')
 
-  track = track_item.css(".base-title").text
-  track = track.gsub(/ \(.+\)/,"")
+    track = track_item.css('.base-title').text
+    track = track.gsub(/ \(.+\)/, '')
 
-  puts artist + " - " + track
-  puts "###################################"
-  }
+    puts artist + ' - ' + track
+    puts '###################################'
+  end
 
-  puts artist + " - " + track
+  puts artist + ' - ' + track
 
-  #puts "Info " + artist
-  puts get_track_info(artist,track)[0]
-  #puts get_track_info("eminem","lose yourself")[1]
+  # puts "Info " + artist
+  puts get_track_info(artist, track)[0]
+  # puts get_track_info("eminem","lose yourself")[1]
 end
 
 # multiple entries or nothing found for #artist - #track:
 # [1] artist - track
 # [2] artist - track
 # [m] manual search
-
