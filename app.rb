005efb7985dfc1841622d8f31f46ef1cba5d5b7e
Chi Trung Nguyen <git@ctnguyen.net>
2019-11-05 09:50:57 +0100

    move scraper to different file

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
-
+require_relative "lib/scrape"
 require 'nokogiri'
 require 'json'
 require 'pry'
@@ -56,37 +56,6 @@ end
 # have consumer reading array from bottom
 #
 song_list = []
-class Scrape
-  def initialize
-    @parsed_page = HTTParty.get("https://hypem.com/napcae")
-    @hypem_loved = Nokogiri::HTML(@parsed_page)
-  end
-
-  def get_artist 
-    # parse loved songs from hypem loved page
-    @hypem_loved.css('#track-list').css('.track_name').reverse.map do |track_item|
-      artist = clean_string(track_item.css('.artist').attribute('title').text)
-    end
-  end
-
-  def get_track 
-    # parse loved songs from hypem loved page
-    @hypem_loved.css('#track-list').css('.track_name').reverse.map do |track_item|
-      track = clean_string(track_item.css('.base-title').text)
-    end
-  end
-end
-
-def worker
-  link, state = get_track_link(artist, track)
-  if !state
-    puts "[#{DATE}]" + "\"" + artist + ' - ' + track + "\" not found."
-  else
-    # push into array
-    a = song_list.push(link)
-  end
-end
-
 
 scraper = Scrape.new
 
