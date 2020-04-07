487a416b9c095fb75de73d22b96c36868bfc6ee8
Chi Trung Nguyen <git@ctnguyen.net>
2020-04-07 16:47:27 +0200

    rubocop

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 class Scrape
   def initialize
-    @parsed_page = HTTParty.get("https://hypem.com/napcae")
+    @parsed_page = HTTParty.get('https://hypem.com/napcae')
     @hypem_loved = Nokogiri::HTML(@parsed_page)
   end
 
-  def get_artist 
+  def get_artist
     # parse loved songs from hypem loved page
     @hypem_loved.css('#track-list').css('.track_name').map do |track_item|
       artist = clean_string(track_item.css('.artist').attribute('title').text)
     end
   end
 
-  def get_track 
+  def get_track
     # parse loved songs from hypem loved page
     @hypem_loved.css('#track-list').css('.track_name').map do |track_item|
       track = clean_string(track_item.css('.base-title').text)
