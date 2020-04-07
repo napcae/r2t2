7e68693138a96081d7b8f15c5eb23932bc4626ff
Chi Trung Nguyen <git@ctnguyen.net>
2020-04-07 17:15:57 +0200

    move get_track_link to helper.rb move object init of Scrape into statement instead of init globally

--- app.rb
+++ app.rb
@@ -17,46 +17,8 @@ DEEZER_API_ENDPOINT = 'https://api.deezer.com/search?q='
 track, artist = ''
 APP_DIR = 'tmp/persistent_queue.json'
 
-DATE = Time.new
 logger = Logger.new(STDOUT)
 logger.level = Logger::DEBUG
-
-# creating api call for deezer, search for artist and tracks scraped from hypem loved page
-# return deezer link for smloadr
-###
-# receives input; artist: name of artist to search for, track: name of track to search for,
-# title_count: number of tracks to return if there are multiple results
-#
-# function returns link, status code as array
-###
-def get_track_link(artist, track, title_count = 1)
-  escape = CGI.escape('artist:' + "\"#{artist}\"" + 'track:' + "\"#{track}\"" + "&limit=#{title_count}&order=RANKING")
-  query = DEEZER_API_ENDPOINT + escape
-
-  deezer_query = HTTP.get(query).to_s
-
-  json = JSON.parse(deezer_query)
-  parsed = json['data']
-  total_songs_count = json['total']
-
-  if total_songs_count.zero?
-    ['', false]
-  else
-    parsed.take(title_count).each do |deezer_search_response|
-      link = deezer_search_response['link']
-      title = deezer_search_response['title']
-      artist_name = deezer_search_response['artist']['name']
-      album_name =  deezer_search_response['album']['title']
-
-      return [link, true, title, artist_name, album_name]
-    end
-  end
-end
-
-scraper = Scrape.new
-
-track = scraper.get_track
-artist = scraper.get_artist
 
 ################################################################
 # this checks whether queue.json exists to reload state after programm is aborted
@@ -67,6 +29,11 @@ if File.file?(APP_DIR)
   persistent_queue = JSON.parse(File.read(APP_DIR))
   worker_queue = persistent_queue
 else
+  scraper = Scrape.new
+
+  track = scraper.get_track
+  artist = scraper.get_artist
+
   # build_tracklist_to_download
   logger.info('persistent_queue.json not found')
   logger.info('Starting up and initializing tracklist...')
@@ -77,8 +44,7 @@ else
     link = get_track_link((artist[index]).to_s, (track[index]).to_s)
     jid = Digest::MD5.hexdigest (artist[index]).to_s + (track[index]).to_s
     temp_hash = {
-      "index": index.to_s,
-      "artist": (artist[index]).to_s,
+      "artist": (artist[index]),
       "track": (track[index]).to_s,
       "link": (link[0]).to_s,
       "jid": jid.to_s,
@@ -97,16 +63,7 @@ else
     # otherwise put in queue
   end
 end
-
 logger.info('Tracklist initialized...')
-
-# while true do
-#   scraper = Scrape.new
-#   track = scraper.get_track
-#   artist = scraper.get_artist
-#   logger.debug("Freshest: #{artist[0]} - #{track[0]}")
-#   sleep 5
-# end
 
 #### main program starts here
 # producer: should create queue.json which holds json representation of hypem.com/napcae + deezer links
@@ -136,7 +93,7 @@ producer = Thread.new do
 
       link = get_track_link((artist[count]).to_s, (track[count]).to_s)
       jid = Digest::MD5.hexdigest (artist[count]).to_s + (track[count]).to_s
-      
+
       temp_hash = {
         'artist' => (artist[count]).to_s,
         'track' => (track[count]).to_s,
@@ -154,9 +111,9 @@ producer = Thread.new do
       logger.debug("Worker Queue: #{persistent_queue[0..3].to_yaml}")
       count += 1
 
-      logger.debug("Attempting to persist changes to disk")
+      logger.debug('Attempting to persist changes to disk')
       File.open(APP_DIR, 'w') do |f|
-       f.write(persistent_queue.to_json)
+        f.write(persistent_queue.to_json)
       end
     end
   end
--- helper.rb
+++ helper.rb
@@ -13,6 +13,38 @@ ensure
   $stderr.reopen original_stderr
 end
 
+# creating api call for deezer, search for artist and tracks scraped from hypem loved page
+# return deezer link for smloadr
+###
+# receives input; artist: name of artist to search for, track: name of track to search for,
+# title_count: number of tracks to return if there are multiple results
+#
+# function returns link, status code as array
+###
+def get_track_link(artist, track, title_count = 1)
+  escape = CGI.escape('artist:' + "\"#{artist}\"" + 'track:' + "\"#{track}\"" + "&limit=#{title_count}&order=RANKING")
+  query = DEEZER_API_ENDPOINT + escape
+
+  deezer_query = HTTP.get(query).to_s
+
+  json = JSON.parse(deezer_query)
+  parsed = json['data']
+  total_songs_count = json['total']
+
+  if total_songs_count.zero?
+    ['', false]
+  else
+    parsed.take(title_count).each do |deezer_search_response|
+      link = deezer_search_response['link']
+      title = deezer_search_response['title']
+      artist_name = deezer_search_response['artist']['name']
+      album_name =  deezer_search_response['album']['title']
+
+      return [link, true, title, artist_name, album_name]
+    end
+  end
+end
+
 def clean_string(str)
   # remove until artist: "SB : COEO"
   str = str.gsub(/.+\:\s/, '')
