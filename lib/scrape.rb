a0f91641d02e9dd0554d475170147514e8d878db
Chi Trung Nguyen <git@ctnguyen.net>
2020-04-08 16:24:05 +0200

    move scrape.rb to lib

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -6,14 +6,14 @@ class Scrape
     @hypem_loved = Nokogiri::HTML(@parsed_page)
   end
 
-  def get_artist
+  def get_artist_from_hypem
     # parse loved songs from hypem loved page
     @hypem_loved.css('#track-list').css('.track_name').map do |track_item|
       artist = clean_string(track_item.css('.artist').attribute('title').text)
     end
   end
 
-  def get_track
+  def get_track_from_hypem
     # parse loved songs from hypem loved page
     @hypem_loved.css('#track-list').css('.track_name').map do |track_item|
       track = clean_string(track_item.css('.base-title').text)
